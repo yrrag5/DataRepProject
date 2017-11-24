6b0936b9081369a4c738333cbbb45f2e65407cd7
yrrag5 <G00335806@gmit.ie>
2017-11-24 16:50:28 +0000

    Tidied up css and made the regurlar expressions more robust for conversations

--- ElizaResponse.go
+++ ElizaResponse.go
@@ -34,14 +34,39 @@ func ElizaResponse(input string) string {
 
     // (?i). for flag, \b before and after 
     
-    // Keyword hello response
-    if matched, _ := regexp.MatchString(`(?i).*\bhello\b.*`, input); matched {
+    // Keyword hello, hi, hey response
+    if matched, _ := regexp.MatchString(`.*\bhello\b.*`, input); matched {
         return "Hello human, how are you doing today?"
-     }
+    }
+
+     if matched, _ := regexp.MatchString(`.*\bhi\b.*`, input); matched {
+        return "Hello human, how are you doing today?"
+    }
+
+     if matched, _ := regexp.MatchString(`.*\bhey\b.*`, input); matched {
+        return "Hello human, how are you doing today?"
+    }
+    //////////////////////////////////////////////////////////////////////////
+
+     // Keyword yourself, you response
+    if matched, _ := regexp.MatchString(`(?i).*\byourself\b.*`, input); matched {
+        return "I am unable to comprehend how I'm feeling as I am a AI."
+    }
+
+     if matched, _ := regexp.MatchString(`(?i).*\byou\b.*`, input); matched {
+        return "I am unable to comprehend how I'm feeling as I am a AI."
+    }
+    ////////////////////////////////////////////////////////////////////////////
+
+    // Keyword sucks response 
+    if matched, _ := regexp.MatchString(`(?i).*\bsucks\b.*`, input); matched {
+        return "It does indeed suck..."
+    }
+    ////////////////////////////////////////////////////////////////////////////
 
     // Keyword father response
      if matched, _ := regexp.MatchString(`(?i).*\bfather\b.*`, input); matched {
-        return "Why don’t you tell me more about your father?"
+        return "Luke I am your father... Sorry, tell me more about your father?"
      }
 
     // Keyword mother response
@@ -51,30 +76,56 @@ func ElizaResponse(input string) string {
 
     // Keyword question response
     if matched, _ := regexp.MatchString(`(?i).*\bquestion\b.*`, input); matched {
-        return "Tell me about where you come from?"
+        return "Tell me about what city you come from?"
     }
 
     // Keyword town response
-    if matched, _ := regexp.MatchString(`(?i).*\btown\b.*`, input); matched {
-        return "Are there any hotspots in your town that you'll like to discuss?"
+    if matched, _ := regexp.MatchString(`(?i).*\bcity\b.*`, input); matched {
+        return "Are there any monuments in your city that you'll like to discuss?"
+    }
+    // keywords for galway memorials 
+    if matched, _ := regexp.MatchString(`(?i).*\bcorrib memorials\b.*`, input); matched {
+        return "Ah the Corrib Memorials very interesting"
     }
 
-     // Keywords star wars response
+     if matched, _ := regexp.MatchString(`(?i).*\bspanish arch\b.*`, input); matched {
+        return "I believe the Spanish Arch is the most traffic heavy hot spot in Galway"
+    }
+
+     if matched, _ := regexp.MatchString(`(?i).*\boranmore castle\b.*`, input); matched {
+        return "That castle is around 500 years old, thats amazing!"
+    }
+
+    //////////////////////////////////////////////////////////////////////////////
+
+     if matched, _ := regexp.MatchString(`(?i).*\bnot sure\b.*`, input); matched {
+        return "Don't worry your not under pressure, if entirely unsure what to ask just ask me a question."
+    }
+
+    if matched, _ := regexp.MatchString(`(?i).*\bprevious visitors\b.*`, input); matched {
+        return "I have no recollection of anyone that may of come before you."
+    }
+
+
+     // Keywords for star wars responses
     if matched, _ := regexp.MatchString(`(?i).*\bstar wars\b.*`, input); matched {
         return "Im glad you noticed the background, are you looking forward to The Last Jedi?"
+    }
+
+     if matched, _ := regexp.MatchString(`(?i).*\bmay the force be with you\b.*`, input); matched {
+        return "And also with you!"
     }
 
     if matched, _ := regexp.MatchString(`(?i).*\bgoodbye\b.*`, input); matched {
         return "May the force be with you!"
         
     }
-
     if matched, _ := regexp.MatchString(`(?i).*\band also with you\b.*`, input); matched {
-        return "haha we did the thing (Type exit to finish)"
+        return "haha we did the thing (Type - exit - to finish)"
     }
 
     // Ends program if exit is entered  
-    if matched, _ := regexp.MatchString(`(?i).*\exit\b.*`, input); matched {
+    if matched, _ := regexp.MatchString(`(?i).*\bexit\b.*`, input); matched {
         os.Exit(3)
     }
 
